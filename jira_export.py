2d7486506c0822b80e438115748c72e30a69e8ba
kjebo <kjellbovin@hotmail.com>
2023-07-05 14:25:27 +0200

    Modified jira_export.py to save the right KPI in database

--- jira_export.py
+++ jira_export.py
@@ -1,7 +1,8 @@
-import json
+import requests
+from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP, Interval, func
+from sqlalchemy.orm import declarative_base, sessionmaker
 from jira import JIRA
 from datetime import datetime
-import requests
 
 options = {
     'server': 'http://localhost:8082/',
@@ -12,62 +13,109 @@ options = {
     'session': requests.Session()
 }
 
-jira = JIRA(options=options)
+# Define the database connection
+engine = create_engine('postgresql://postgres:postgres@192.168.68.100/postgres')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class JiraExport(Base):
+    __tablename__ = 'jira_export'
+    changelog_id = Column(Integer, primary_key=True)
+    issue_id = Column(Integer)
+    issuetype = Column(String)
+    changelog_from = Column(String)
+    changelog_to = Column(String)
+    previous_update = Column(TIMESTAMP)
+    this_update = Column(TIMESTAMP)
+    duration = Column(Interval)
+
 
 class Issue:
-    def __init__(self, id, key, issuetype, parent, subtasks, created, resolutiondate, changelog):
+    def __init__(self, id, issuetype, changelog):
         self.id = id
-        self.key = key
         self.issuetype = issuetype
-        self.parent = parent
-        self.subtasks = subtasks
-        self.created = created
-        self.resolutiondate = resolutiondate
         self.changelog = changelog
 
     def __str__(self):
-        return f"ID: {self.id}\nKey: {self.key}\nType: {self.issuetype}\nParent: {self.parent}\nSubtasks: {self.subtasks}\nCreated: {self.created}\nResolution Date: {self.resolutiondate}\nChangelog: {self.changelog}"
+        return f"ID: {self.id}\nType: {self.issuetype}\nChangelog: {self.changelog}"
 
-try:
-    issues = jira.search_issues('')
 
-    issue_list = []
+def fetch_issues():
+    try:
+        jira = JIRA(options=options)
+        issues = jira.search_issues('')
+        return jira, issues
+    except Exception as e:
+        print(f"Failed to fetch issues: {e}")
+        return None, []
 
-    for issue in issues:
-        issue_id = issue.id
-        issue_key = issue.key
-        issue_data = jira.issue(issue_key, expand='changelog')
 
-        created_date_str = issue_data.fields.created
-        created_date = datetime.strptime(created_date_str, "%Y-%m-%dT%H:%M:%S.%f%z")
-        resolution_date_str = issue_data.fields.resolutiondate
-        resolution_date = datetime.strptime(resolution_date_str, "%Y-%m-%dT%H:%M:%S.%f%z") if resolution_date_str else None
+def process_issues(jira, issues):
+    try:
+        for issue in issues:
+            issue_id = issue.id
+            issue_data = jira.issue(issue.key, expand='changelog')
+            changelog = extract_changelog(issue_data)
 
-        parent_key = issue_data.fields.parent.key if hasattr(issue_data.fields, 'parent') and issue_data.fields.parent else None
+            previous_update = None  # Initialize previous_update to None for the first changelog item
 
-        subtasks = [subtask.key for subtask in jira.search_issues(f'parent={issue_key}')]
+            for changelog_item in changelog:
+                changelog_id = changelog_item['Changelog ID']
+                existing_entry = session.query(JiraExport).filter_by(changelog_id=changelog_id).first()
 
-        if not subtasks:
-            subtasks = None
+                if existing_entry:
+                    existing_entry.changelog_from = changelog_item['From']
+                    existing_entry.changelog_to = changelog_item['To']
+                    existing_entry.this_update = changelog_item['Created']
+                    if existing_entry.previous_update:
+                        previous_update = func.timezone('UTC', existing_entry.previous_update)  # Convert previous_update to UTC timezone
+                        duration = existing_entry.this_update - previous_update
+                        existing_entry.duration = duration
+                else:
+                    jira_export = JiraExport(
+                        issue_id=issue_id,
+                        changelog_id=changelog_id,
+                        issuetype=issue_data.fields.issuetype.name,
+                        changelog_from=changelog_item['From'],
+                        changelog_to=changelog_item['To'],
+                        previous_update=previous_update,
+                        this_update=changelog_item['Created'],
+                    )
 
-        changelog = []
-        for history in issue_data.changelog.histories:
-            for item in history.items:
-                if item.fromString in ['In Review', 'To Do', 'In Progress', 'Done'] and item.toString in ['In Review', 'To Do', 'In Progress', 'Done']:
-                    created_timestamp = history.created
-                    changelog.append({
-                        'Created': created_timestamp,
-                        'From': item.fromString,
-                        'To': item.toString,
-                    })
+                    if previous_update is not None:
+                        duration = changelog_item['Created'] - previous_update
+                        jira_export.duration = duration
 
-        issue = Issue(issue_id, issue_key, issue_data.fields.issuetype.name, parent_key, subtasks, created_date_str, resolution_date_str, changelog)
-        issue_list.append(issue)
+                    previous_update = changelog_item['Created']  # Update the previous update
 
-    with open('issues.json', 'w') as file:
-        json.dump([issue.__dict__ for issue in issue_list], file, indent=4)
+                    session.add(jira_export)
 
-    print("issues.json file created successfully.")
+        session.commit()
+        print("Data inserted/updated successfully into the PostgreSQL database.")
+    except Exception as e:
+        session.rollback()
+        print(f"Failed to insert/update data: {e}")
+    finally:
+        session.close()
 
-except Exception as e:
-    print(f"Failed to fetch issues: {e}")
+
+def extract_changelog(issue_data):
+    changelog = []
+    for history in issue_data.changelog.histories:
+        for item in history.items:
+            created_timestamp = datetime.strptime(history.created, "%Y-%m-%dT%H:%M:%S.%f%z")
+            changelog.append({
+                'Changelog ID': history.id,
+                'Created': created_timestamp,
+                'From': item.fromString,
+                'To': item.toString,
+            })
+    return changelog
+
+
+if __name__ == '__main__':
+    jira, issues = fetch_issues()
+    if jira is not None:
+        process_issues(jira, issues)
